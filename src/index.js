c19addcddf9281262132550dce73a134fd0bceeb
alina <alina.rashid2002@gmail.com>
2024-04-28 00:19:01 +0400

    added displayChoice function which adds the player's symbol to the grid

--- src/index.js
+++ src/index.js
@@ -6,18 +6,23 @@ function Player(name, symbol) {
 }
 
 const Game = (function () {
-  let gameBoard = ['X','O','O','O','O','X','X','X','X'];
-  const player1 = Player("alina", 'X');
-  const player2 = Player("zara", 'O');
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
+  const player1 = Player("alina", "X");
+  const player2 = Player("zara", "O");
 
   function player1MarkPosition(position) {
-    this.gameBoard.splice(position, 1,'X'); //remove available position from board
+    if (gameBoard[position] != "O") {
+      this.gameBoard.splice(position, 1, "X"); //remove available position from board
+    }
   }
 
   function player2MarkPosition(position) {
-    this.gameBoard.splice(position, 1,'O'); //remove available position from board
+    if (gameBoard[position] != "X") {
+      this.gameBoard.splice(position, 1, "O"); //remove available position from board
+    }
   }
 
+  //2d matrix of all the winning positions
   const correctPositions = [
     [0, 4, 8],
     [0, 1, 2],
@@ -29,38 +34,32 @@ const Game = (function () {
     [6, 7, 8],
   ];
 
-
   function checkWinner() {
-    for(let i=0; i<correctPositions.length; i++){
-        var player1Counter=0;
-        var player2Counter = 0;
-        for(let j=0; j<correctPositions[i].length; j++){
-            let index=correctPositions[i][j];
-            if(this.gameBoard[index]=='X'){
-                player1Counter++;
-                console.log("player1Counter: "+ player1Counter);
-                console.log("index: "+ index);
-            }
-            else if(this.gameBoard[index]=='O'){
-                player2Counter++;
-                console.log("player2counter: "+ player2Counter);
-                console.log("index: "+ index);
-            }
+    for (let i = 0; i < correctPositions.length; i++) {
+      var player1Counter = 0; //no. of times a player chooses a position
+      var player2Counter = 0;
+      for (let j = 0; j < correctPositions[i].length; j++) {
+        let index = correctPositions[i][j];
+        if (this.gameBoard[index] == "X") {
+          player1Counter++; //if player matches the position their count increments
+          console.log("player1Counter: " + player1Counter);
+          console.log("index: " + index);
+        } else if (this.gameBoard[index] == "O") {
+          player2Counter++;
+          console.log("player2counter: " + player2Counter);
+          console.log("index: " + index);
         }
-        
+      }
     }
-    if(player1Counter==3){
-        return player1.name;
-    }
-    else if(player2Counter==3){
-        return player2.name;
-    }
-    else if(player1Counter==player2Counter){
-        return "tie";
+    if (player1Counter == 3) {
+      //if player matches all three positions in one array they win
+      return player1.name;
+    } else if (player2Counter == 3) {
+      return player2.name;
+    } else if (player1Counter == player2Counter) {
+      return "tie";
     }
   }
-
- 
 
   return {
     gameBoard,
@@ -68,6 +67,34 @@ const Game = (function () {
     player2,
     player1MarkPosition,
     player2MarkPosition,
-    checkWinner
+    checkWinner,
   };
 })();
+
+//allows players to take turns
+let player1_turn = true;
+
+function displayChoice(e) {
+  if (player1_turn) {
+    Game.player1MarkPosition(e.target.id);
+    player1_turn=false;
+  }
+  else{
+    Game.player2MarkPosition(e.target.id);
+    player1_turn=true;
+  }
+
+  const grid = Array.from(document.querySelectorAll(".div"));
+  const board = Game.gameBoard;
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].innerText = board[i];
+  }
+  console.log(board);
+}
+
+
+//gets all the divs in grid and attaches displayChoice function to display symbols for each player
+const buttons = Array.from(document.querySelectorAll(".div"));
+buttons.forEach((elem) => {
+  elem.addEventListener("click", displayChoice);
+});
