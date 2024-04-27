75a9420aee1bac3c10a9599310a924278a81a6da
alina <alina.rashid2002@gmail.com>
2024-04-28 01:06:06 +0400

    added checkWinner() function from Game into displayChoice() function to keep track of the winning player and display final result

--- src/index.js
+++ src/index.js
@@ -36,27 +36,32 @@ const Game = (function () {
 
   function checkWinner() {
     for (let i = 0; i < correctPositions.length; i++) {
-      var player1Counter = 0; //no. of times a player chooses a position
+      var player1Counter = 0;        //no. of times a player chooses a position
       var player2Counter = 0;
       for (let j = 0; j < correctPositions[i].length; j++) {
         let index = correctPositions[i][j];
         if (this.gameBoard[index] == "X") {
           player1Counter++; //if player matches the position their count increments
-          console.log("player1Counter: " + player1Counter);
-          console.log("index: " + index);
+          console.log("index: " + index + " counter: "+ player1Counter);
         } else if (this.gameBoard[index] == "O") {
           player2Counter++;
-          console.log("player2counter: " + player2Counter);
-          console.log("index: " + index);
+          //console.log("player2counter: " + player2Counter);
+          //console.log("index: " + index);
         }
       }
+      if(player1Counter==3){
+        break;
+      }
+      else if(player2Counter==3){
+        break;
+    }
     }
     if (player1Counter == 3) {
       //if player matches all three positions in one array they win
       return player1.name;
     } else if (player2Counter == 3) {
       return player2.name;
-    } else if (player1Counter == player2Counter) {
+    } else{
       return "tie";
     }
   }
@@ -90,11 +95,25 @@ function displayChoice(e) {
     grid[i].innerText = board[i];
   }
   console.log(board);
+  const array_of_results=[];
+  array_of_results.push(Game.checkWinner());
+  const result=document.querySelector('.result');
+  if(array_of_results.includes(Game.player1.name)){
+    result.innerText=Game.player1.name;
+  }
+  else if(array_of_results.includes(Game.player2.name)){
+    result.innerText=Game.player2.name;
+  }
+  else{
+    result.innerText="tie";
+  }
+  
 }
-
 
 //gets all the divs in grid and attaches displayChoice function to display symbols for each player
 const buttons = Array.from(document.querySelectorAll(".div"));
 buttons.forEach((elem) => {
   elem.addEventListener("click", displayChoice);
 });
+
+
